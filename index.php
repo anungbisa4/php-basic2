fd29abaa88645529cd74095f9f2512f131c44c74
anungbisa4 <anung.bisa4@gmail.com>
2017-10-05 21:24:18 +0700

    task array

--- index.php
+++ index.php
@@ -5,8 +5,31 @@
     <title>PHP</title>
   </head>
   <body>
-    <?php
-      echo "<h1>HELLO PHP!</h1>";
-     ?>
+    <!-- <?php
+      // echo "<h1>HELLO PHP!</h1>";
+     ?> -->
+
+     <body>
+
+      <?php
+          // 8,5,4,3,0
+         /* First method to create array. */
+         $numbers = array( 1, 2, 3, 4, 5, 6);
+         $numbers2= array( 8,5,4,3,0);
+         array_splice($numbers, 0, 1, $numbers2);
+         array_pop($numbers);
+         array_pop($numbers);
+         array_pop($numbers);
+         array_pop($numbers);
+         array_pop($numbers);
+        //  array_push($numbers, 0)
+        //  array_shift($numbers);
+        //  array_shift($numbers);
+         var_dump($numbers);
+        //  array_push($numbers, 5, 6);
+        //  array_pop($numbers)
+
+
+         ?>
   </body>
 </html>
